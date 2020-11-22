97aa6555d6b7c2c7425253c6140226d20c151f67
Christine Smith <christinecsmith1@gmail.com>
2020-11-22 13:26:34 -0800

    mvp attempt 1

--- adv.py
+++ adv.py
@@ -10,10 +10,10 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/test_line.txt"
+#map_file = "maps/test_cross.txt"
+#map_file = "maps/test_loop.txt"
+#map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -29,6 +29,42 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+visited = set()
+
+backwards_path = []
+
+backwards_directions = {
+    "n" : "s",
+    "s" : "n", 
+    "w" : "e",
+    "e" : "w"
+}
+
+
+# run while visited is less than the number of rooms in the world
+while len(visited) < len(world.rooms):
+    # print('current room', player.current_room.id)
+    next_move = None
+    possible_exits = []
+
+    for direction in player.current_room.get_exits():
+        if player.current_room.get_room_in_direction(direction) not in visited:
+            possible_exits.append(direction)
+        
+    for direction in possible_exits:
+            next_move = random.choice(possible_exits)
+            break
+
+    if next_move is not None:
+        traversal_path.append(next_move)
+        backwards_path.append(backwards_directions[next_move])
+        player.travel(next_move, True)
+        visited.add(player.current_room)
+    
+    else:
+        next_move = backwards_path.pop()
+        traversal_path.append(next_move)
+        player.travel(next_move)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
